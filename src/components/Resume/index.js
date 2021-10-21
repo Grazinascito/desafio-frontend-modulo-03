4b5ad912be80a8879dcf5c486998c99fd500e2b6
GraziNascito <grazi.social84@gmail.com>
2021-10-20 22:17:28 -0300

    add resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,19 +3,19 @@ import "./style.css";
 const Resume = () => {
   return (
     <div className="container-resume">
-      <h3>Resumo</h3>
+      <h3 >Resumo</h3>
 
       <div className="container-resume__itens">
         <div>Entradas</div>
-        <div>R$ 200,00</div>
+        <div className="container-resume__itens--blueocean">R$ 2222200,00</div>
       </div>
       <div className="container-resume__itens">
         <div>Saídas</div>
-        <div>R$ 70,50</div>
+        <div className="container-resume__itens--orange">R$ 2222270,50</div>
       </div>
       <div className="container-resume__itens">
         <div className="container-resume__itens--bold">Saldo</div>
-        <div className="container-resume__itens--blueocean">R$ 129,50</div>
+        <div className="container-resume__itens--blue">R$ 22222129,50</div>
       </div>
     </div>
   );
--- src/pages/Main/App.js
+++ src/pages/Main/App.js
@@ -1,17 +1,21 @@
-
-import './App.css';
-import Header from '../../components/Header';
-import TransactionsTable from '../../components/TransactionsTable';
-import Resume from '../../components/Resume';
+import "./App.css";
+import Header from "../../components/Header";
+import TransactionsTable from "../../components/TransactionsTable";
+import Resume from "../../components/Resume";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <main>
-          <TransactionsTable/>
+      <Header />
+      <main>
+        <TransactionsTable />
+        <div>
           <Resume />
-        </main>
+          <div>
+            <button className="btn-add">Adicionar Registro</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
